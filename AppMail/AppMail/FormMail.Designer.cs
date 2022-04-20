f936fb0af6199cedd8e494c3e68f4a211929b5e7
joaovlealc <99933075+joaovlealc@users.noreply.github.com>
2022-04-20 20:38:57 -0300

    Add files via upload

---
+++ AppMail/AppMail/FormMail.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace AppMail
+{
+    partial class FormMail
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lblPara = new System.Windows.Forms.Label();
+            this.txtPara = new System.Windows.Forms.TextBox();
+            this.txtCc = new System.Windows.Forms.TextBox();
+            this.lblCc = new System.Windows.Forms.Label();
+            this.txtAssunto = new System.Windows.Forms.TextBox();
+            this.lblAssunto = new System.Windows.Forms.Label();
+            this.txtAnexo = new System.Windows.Forms.TextBox();
+            this.lblAnexo = new System.Windows.Forms.Label();
+            this.btnAnexos = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.Color.YellowGreen;
+            this.btnEnviar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(12, 12);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 85);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lblPara
+            // 
+            this.lblPara.AutoSize = true;
+            this.lblPara.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPara.Location = new System.Drawing.Point(109, 12);
+            this.lblPara.Name = "lblPara";
+            this.lblPara.Size = new System.Drawing.Size(33, 13);
+            this.lblPara.TabIndex = 1;
+            this.lblPara.Text = "Para";
+            // 
+            // txtPara
+            // 
+            this.txtPara.Location = new System.Drawing.Point(112, 28);
+            this.txtPara.Name = "txtPara";
+            this.txtPara.Size = new System.Drawing.Size(313, 20);
+            this.txtPara.TabIndex = 2;
+            // 
+            // txtCc
+            // 
+            this.txtCc.Location = new System.Drawing.Point(112, 77);
+            this.txtCc.Name = "txtCc";
+            this.txtCc.Size = new System.Drawing.Size(313, 20);
+            this.txtCc.TabIndex = 4;
+            // 
+            // lblCc
+            // 
+            this.lblCc.AutoSize = true;
+            this.lblCc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCc.Location = new System.Drawing.Point(109, 61);
+            this.lblCc.Name = "lblCc";
+            this.lblCc.Size = new System.Drawing.Size(22, 13);
+            this.lblCc.TabIndex = 3;
+            this.lblCc.Text = "Cc";
+            // 
+            // txtAssunto
+            // 
+            this.txtAssunto.Location = new System.Drawing.Point(12, 129);
+            this.txtAssunto.Name = "txtAssunto";
+            this.txtAssunto.Size = new System.Drawing.Size(413, 20);
+            this.txtAssunto.TabIndex = 6;
+            // 
+            // lblAssunto
+            // 
+            this.lblAssunto.AutoSize = true;
+            this.lblAssunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAssunto.Location = new System.Drawing.Point(9, 113);
+            this.lblAssunto.Name = "lblAssunto";
+            this.lblAssunto.Size = new System.Drawing.Size(52, 13);
+            this.lblAssunto.TabIndex = 5;
+            this.lblAssunto.Text = "Assunto";
+            // 
+            // txtAnexo
+            // 
+            this.txtAnexo.Location = new System.Drawing.Point(12, 179);
+            this.txtAnexo.Name = "txtAnexo";
+            this.txtAnexo.Size = new System.Drawing.Size(413, 20);
+            this.txtAnexo.TabIndex = 8;
+            // 
+            // lblAnexo
+            // 
+            this.lblAnexo.AutoSize = true;
+            this.lblAnexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnexo.Location = new System.Drawing.Point(9, 163);
+            this.lblAnexo.Name = "lblAnexo";
+            this.lblAnexo.Size = new System.Drawing.Size(42, 13);
+            this.lblAnexo.TabIndex = 7;
+            this.lblAnexo.Text = "Anexo";
+            // 
+            // btnAnexos
+            // 
+            this.btnAnexos.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnAnexos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnexos.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnexos.Location = new System.Drawing.Point(431, 179);
+            this.btnAnexos.Name = "btnAnexos";
+            this.btnAnexos.Size = new System.Drawing.Size(75, 24);
+            this.btnAnexos.TabIndex = 11;
+            this.btnAnexos.Text = "Anexos";
+            this.btnAnexos.UseVisualStyleBackColor = false;
+            this.btnAnexos.Click += new System.EventHandler(this.btnAnexos_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.Location = new System.Drawing.Point(9, 215);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(67, 13);
+            this.lblMensagem.TabIndex = 12;
+            this.lblMensagem.Text = "Mensagem";
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(12, 231);
+            this.txtMensagem.Multiline = true;
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(494, 187);
+            this.txtMensagem.TabIndex = 13;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Red;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(12, 439);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 24);
+            this.btnSair.TabIndex = 14;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FormMail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(531, 478);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnAnexos);
+            this.Controls.Add(this.txtAnexo);
+            this.Controls.Add(this.lblAnexo);
+            this.Controls.Add(this.txtAssunto);
+            this.Controls.Add(this.lblAssunto);
+            this.Controls.Add(this.txtCc);
+            this.Controls.Add(this.lblCc);
+            this.Controls.Add(this.txtPara);
+            this.Controls.Add(this.lblPara);
+            this.Controls.Add(this.btnEnviar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mail";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label lblPara;
+        private System.Windows.Forms.TextBox txtPara;
+        private System.Windows.Forms.TextBox txtCc;
+        private System.Windows.Forms.Label lblCc;
+        private System.Windows.Forms.TextBox txtAssunto;
+        private System.Windows.Forms.Label lblAssunto;
+        private System.Windows.Forms.TextBox txtAnexo;
+        private System.Windows.Forms.Label lblAnexo;
+        private System.Windows.Forms.Button btnAnexos;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ AppMail/AppMail/FormMail.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace AppMail
+{
+    public partial class FormMail : Form
+    {
+        public FormMail()
+        {
+            InitializeComponent();
+        }
+
+        public void SendMail()
+        {
+            MailMessage message = new MailMessage();
+            message.To.Add(txtPara.Text);
+            message.CC.Add(txtCc.Text);
+            Attachment anexo = new Attachment(txtAnexo.Text);
+            message.Attachments.Add(anexo);
+            message.Subject = txtAssunto.Text;
+            message.From = new MailAddress("XXXX@hotmail.com");
+            message.Body = txtMensagem.Text;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.office365.com";
+            smtp.EnableSsl = true;
+            message.Priority = MailPriority.High;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new System.Net.NetworkCredential("XXXX@hoymail.com", "password");
+            try
+            {
+                smtp.Send(message);
+                MessageBox.Show("E-mail enviado com sucesso!");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            SendMail();
+        }
+
+        private void btnAnexos_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            txtAnexo.Text = openFileDialog1.FileName;
+        }
+    }
+}
